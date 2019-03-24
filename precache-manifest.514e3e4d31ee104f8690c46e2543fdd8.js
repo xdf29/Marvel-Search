dc2ba8cdf716281f95f7f8ca23f88f088f9d8952
XuanD <xuandeng1029@gmail.com>
2019-03-24 21:44:35 +0800

    Updates

---
+++ precache-manifest.514e3e4d31ee104f8690c46e2543fdd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31a664a45056ff0fdd11",
+    "url": "/Marvel-Search/static/js/runtime~main.ca7b9fcc.js"
+  },
+  {
+    "revision": "bfcedad0df76d807ff43",
+    "url": "/Marvel-Search/static/js/main.36301629.chunk.js"
+  },
+  {
+    "revision": "0f284e84f71bac71f964",
+    "url": "/Marvel-Search/static/js/2.ee308871.chunk.js"
+  },
+  {
+    "revision": "bfcedad0df76d807ff43",
+    "url": "/Marvel-Search/static/css/main.20d8a807.chunk.css"
+  },
+  {
+    "revision": "0f284e84f71bac71f964",
+    "url": "/Marvel-Search/static/css/2.6586e681.chunk.css"
+  },
+  {
+    "revision": "44ba8dece0e2e05dcd0d598abab4189a",
+    "url": "/Marvel-Search/index.html"
+  }
+];
